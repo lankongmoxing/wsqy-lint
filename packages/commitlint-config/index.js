b39232dc640f09fcf201f3f8a28d69c55c098279
lanyunqing <lanyunqing@yco.pet>
2024-09-25 15:46:04 +0800

    Mod: 1

--- packages/commitlint-config/index.js
+++ packages/commitlint-config/index.js
@@ -12,6 +12,6 @@ module.exports = {
     'subject-full-stop': [2, 'never', '.'],
     'type-case': [2, 'always', 'lower-case'],
     'type-empty': [2, 'never'],
-    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'test', 'refactor', 'chore', 'revert', 'x']],
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'test', 'refactor', 'chore', 'revert', 'x1']],
   },
 };
